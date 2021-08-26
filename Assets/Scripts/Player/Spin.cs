fcaf87753bd7e63370ee81a5045309865b8d2920
sawada14 <Msandfrey@sbcglobal.net>
2021-08-25 22:39:46 -0700

    Ship go spin

---
+++ Assets/Scripts/Player/Spin.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IndieWizards.Player
+{
+    public class Spin : MonoBehaviour
+    {
+        private int spinCount = 0;
+        private bool isSpinning = false;
+        private float startRotation = 0;
+        private float endRotation = 360;
+        [SerializeField]
+        private float currRotation;
+        [SerializeField]
+        private float rotationStep;
+        [SerializeField]
+        private GameObject Player;
+        // Start is called before the first frame update
+        // Update is called once per frame
+        void Update()
+        {
+            if (isSpinning && spinCount > 0)
+            {
+                //spin
+                if(currRotation >= endRotation)
+                {
+                    spinCount--;
+                    isSpinning = spinCount > 0;
+                }
+                else
+                {
+                    transform.Rotate(new Vector3(0, rotationStep * Time.deltaTime, 0));
+                    currRotation += rotationStep * Time.deltaTime;
+                    currRotation = Mathf.Clamp(currRotation, 0, 360);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Q)) { BeASpin(); }
+        }
+
+        public void BeASpin()
+        {
+            if (isSpinning) { return; }
+            currRotation = startRotation;
+            spinCount++;
+            isSpinning = true;
+        }
+    }
+}
